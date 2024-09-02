dde30107b5b2845d0498eeb9861fd44cc78c3357
Daniel <Smileybob72801@gmail.com>
2024-09-02 04:46:37 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace NumericTypeSuggestor
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			MinValueLabel = new Label();
+			MaxValueLabel = new Label();
+			MinValueTextBox = new TextBox();
+			MaxValueTextBox = new TextBox();
+			IntegralOnlyCheckBox = new CheckBox();
+			MustBePreciseCheckBox = new CheckBox();
+			label3 = new Label();
+			TypeLabel = new Label();
+			SuspendLayout();
+			// 
+			// MinValueLabel
+			// 
+			MinValueLabel.AutoSize = true;
+			MinValueLabel.Font = new Font("Segoe UI", 20F);
+			MinValueLabel.Location = new Point(45, 50);
+			MinValueLabel.Name = "MinValueLabel";
+			MinValueLabel.Size = new Size(141, 37);
+			MinValueLabel.TabIndex = 0;
+			MinValueLabel.Text = "Min Value:";
+			// 
+			// MaxValueLabel
+			// 
+			MaxValueLabel.AutoSize = true;
+			MaxValueLabel.Font = new Font("Segoe UI", 20F);
+			MaxValueLabel.Location = new Point(41, 98);
+			MaxValueLabel.Name = "MaxValueLabel";
+			MaxValueLabel.Size = new Size(145, 37);
+			MaxValueLabel.TabIndex = 1;
+			MaxValueLabel.Text = "Max Value:";
+			// 
+			// MinValueTextBox
+			// 
+			MinValueTextBox.Font = new Font("Segoe UI", 20F);
+			MinValueTextBox.Location = new Point(192, 47);
+			MinValueTextBox.Name = "MinValueTextBox";
+			MinValueTextBox.Size = new Size(581, 43);
+			MinValueTextBox.TabIndex = 2;
+			MinValueTextBox.TextChanged += MinValueTextBox_TextChanged;
+			MinValueTextBox.KeyPress += MinValueTextBox_KeyPress;
+			// 
+			// MaxValueTextBox
+			// 
+			MaxValueTextBox.Font = new Font("Segoe UI", 20F);
+			MaxValueTextBox.Location = new Point(192, 95);
+			MaxValueTextBox.Name = "MaxValueTextBox";
+			MaxValueTextBox.Size = new Size(581, 43);
+			MaxValueTextBox.TabIndex = 3;
+			MaxValueTextBox.TextChanged += MaxValueTextBox_TextChanged;
+			MaxValueTextBox.KeyPress += MaxValueTextBox_KeyPress;
+			// 
+			// IntegralOnlyCheckBox
+			// 
+			IntegralOnlyCheckBox.AutoSize = true;
+			IntegralOnlyCheckBox.CheckAlign = ContentAlignment.MiddleRight;
+			IntegralOnlyCheckBox.Font = new Font("Segoe UI", 14F);
+			IntegralOnlyCheckBox.Location = new Point(45, 195);
+			IntegralOnlyCheckBox.Name = "IntegralOnlyCheckBox";
+			IntegralOnlyCheckBox.Size = new Size(149, 29);
+			IntegralOnlyCheckBox.TabIndex = 4;
+			IntegralOnlyCheckBox.Text = "Integral Only?";
+			IntegralOnlyCheckBox.TextAlign = ContentAlignment.MiddleRight;
+			IntegralOnlyCheckBox.UseVisualStyleBackColor = true;
+			IntegralOnlyCheckBox.CheckedChanged += IntegralOnlyCheckBox_CheckedChanged;
+			// 
+			// MustBePreciseCheckBox
+			// 
+			MustBePreciseCheckBox.AutoSize = true;
+			MustBePreciseCheckBox.CheckAlign = ContentAlignment.MiddleRight;
+			MustBePreciseCheckBox.Font = new Font("Segoe UI", 14F);
+			MustBePreciseCheckBox.Location = new Point(94, 242);
+			MustBePreciseCheckBox.Name = "MustBePreciseCheckBox";
+			MustBePreciseCheckBox.Size = new Size(100, 29);
+			MustBePreciseCheckBox.TabIndex = 5;
+			MustBePreciseCheckBox.Text = "Precise?";
+			MustBePreciseCheckBox.TextAlign = ContentAlignment.MiddleRight;
+			MustBePreciseCheckBox.UseVisualStyleBackColor = true;
+			MustBePreciseCheckBox.CheckedChanged += MustBePreciseCheckBox_CheckedChanged;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+			label3.Location = new Point(107, 318);
+			label3.Name = "label3";
+			label3.Size = new Size(86, 37);
+			label3.TabIndex = 6;
+			label3.Text = "Type:";
+			// 
+			// TypeLabel
+			// 
+			TypeLabel.AutoSize = true;
+			TypeLabel.Font = new Font("Segoe UI", 20F, FontStyle.Bold);
+			TypeLabel.Location = new Point(192, 318);
+			TypeLabel.Name = "TypeLabel";
+			TypeLabel.Size = new Size(230, 37);
+			TypeLabel.TabIndex = 7;
+			TypeLabel.Text = "not enough data";
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(TypeLabel);
+			Controls.Add(label3);
+			Controls.Add(MustBePreciseCheckBox);
+			Controls.Add(IntegralOnlyCheckBox);
+			Controls.Add(MaxValueTextBox);
+			Controls.Add(MinValueTextBox);
+			Controls.Add(MaxValueLabel);
+			Controls.Add(MinValueLabel);
+			Name = "Form1";
+			Text = "Numeric Type Suggester";
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Label MinValueLabel;
+		private Label MaxValueLabel;
+		private TextBox MinValueTextBox;
+		private TextBox MaxValueTextBox;
+		private CheckBox IntegralOnlyCheckBox;
+		private CheckBox MustBePreciseCheckBox;
+		private Label label3;
+		private Label TypeLabel;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,184 @@
+using Microsoft.VisualBasic;
+using System.Numerics;
+
+namespace NumericTypeSuggestor
+{
+	public partial class Form1 : Form
+	{
+		private BigInteger _minValue;
+		private BigInteger _maxValue;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void IntegralOnlyCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			bool IsChecked = IntegralOnlyCheckBox.Checked;
+
+			if (IsChecked)
+			{
+				MustBePreciseCheckBox.Checked = false;
+			}
+
+			MustBePreciseCheckBox.Visible = !IsChecked;
+
+			SetSuggestedTypeText(_minValue, _maxValue);
+		}
+
+		private void MustBePreciseCheckBox_CheckedChanged(object sender, EventArgs e)
+		{
+			SetSuggestedTypeText(_minValue, _maxValue);
+		}
+
+		private void MinValueTextBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (MinValueTextBox.Text.Length == 0)
+			{
+				if (!IsValidCharOrMinus(e.KeyChar))
+				{
+					e.Handled = true;
+				}
+			}
+			else if (!IsValidChar(e.KeyChar))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private void MaxValueTextBox_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (MaxValueTextBox.Text.Length == 0)
+			{
+				if (!IsValidCharOrMinus(e.KeyChar))
+				{
+					e.Handled = true;
+				}
+			}
+			else if (!IsValidChar(e.KeyChar))
+			{
+				e.Handled = true;
+			}
+		}
+
+		private static bool IsValidChar(char keyChar) =>
+			char.IsControl(keyChar) || char.IsDigit(keyChar);
+
+		private static bool IsValidCharOrMinus(char keyChar) =>
+			keyChar.Equals('-') || char.IsControl(keyChar) || char.IsDigit(keyChar);
+
+		private void SetSuggestedTypeText(BigInteger min, BigInteger max)
+		{
+			if (min > max)
+				TypeLabel.Text = "min cannot be larger than max";
+			else if (MinValueTextBox.Text.Length < 1 || MaxValueTextBox.Text.Length < 1)
+				TypeLabel.Text = "not enough data";
+			else if (IntegralOnlyCheckBox.Checked)
+			{
+				if (min >= byte.MinValue && max <= byte.MaxValue)
+					TypeLabel.Text = "byte";
+				else if (min >= sbyte.MinValue && max <= sbyte.MaxValue)
+					TypeLabel.Text = "sbyte";
+				else if (min >= ushort.MinValue && max <= ushort.MaxValue)
+					TypeLabel.Text = "ushort";
+				else if (min >= short.MinValue && max <= short.MaxValue)
+					TypeLabel.Text = "short";
+				else if (min >= uint.MinValue && max <= uint.MaxValue)
+					TypeLabel.Text = "uint";
+				else if (min >= int.MinValue && max <= int.MaxValue)
+					TypeLabel.Text = "int";
+				else if (min >= ulong.MinValue && max <= ulong.MaxValue)
+					TypeLabel.Text = "ulong";
+				else if (min >= long.MinValue && max <= long.MaxValue)
+					TypeLabel.Text = "long";
+				else
+					TypeLabel.Text = "BigInteger";
+			}
+			else if (!IntegralOnlyCheckBox.Checked)
+			{
+				_ = double.TryParse(MinValueTextBox.Text, out double minAsDouble);
+				_ = double.TryParse(MaxValueTextBox.Text, out double maxAsDouble);
+
+				bool minDecimalParsed = decimal.TryParse(MinValueTextBox.Text, out decimal minAsDecimal);
+				bool maxDecimalParsed = decimal.TryParse(MaxValueTextBox.Text, out decimal maxAsDecimal);
+
+				if (!MustBePreciseCheckBox.Checked)
+				{
+					if (minAsDouble >= float.MinValue && maxAsDouble <= float.MaxValue)
+						TypeLabel.Text = "float";
+					else if (minAsDouble >= double.MinValue && maxAsDouble <= double.MaxValue)
+						TypeLabel.Text = "double";
+					else
+						TypeLabel.Text = "Impossible representation";
+				}
+				else
+				{
+					if (minDecimalParsed &&
+						maxDecimalParsed &&
+						minAsDecimal >= decimal.MinValue &&
+						maxAsDecimal <= decimal.MaxValue)
+						TypeLabel.Text = "decimal";
+					else
+						TypeLabel.Text = "Impossible representation";
+				}
+			}
+		}
+
+		private void MinValueTextBox_TextChanged(object sender, EventArgs e)
+		{
+			if (MinValueTextBox.Text.Length > 0)
+			{
+				bool wasParsed = BigInteger.TryParse(MinValueTextBox.Text, out BigInteger result);
+
+				if (wasParsed)
+				{
+					_minValue = result;
+				}
+			}
+			else
+			{
+				_minValue = 0;
+			}
+
+			if (_minValue > _maxValue)
+			{
+				MaxValueTextBox.BackColor = Color.Red;
+			}
+			else
+			{
+				MaxValueTextBox.BackColor = Color.White;
+			}
+
+			SetSuggestedTypeText(_minValue, _maxValue);
+		}
+
+		private void MaxValueTextBox_TextChanged(object sender, EventArgs e)
+		{
+			if (MaxValueTextBox.Text.Length > 0)
+			{
+				bool wasParsed = BigInteger.TryParse(MaxValueTextBox.Text, out BigInteger result);
+
+				if (wasParsed)
+				{
+					_maxValue = result;
+				}
+			}
+			else
+			{
+				_maxValue = 0;
+			}
+
+			if (_minValue > _maxValue)
+			{
+				MaxValueTextBox.BackColor = Color.Red;
+			}
+			else
+			{
+				MaxValueTextBox.BackColor = Color.White;
+			}
+
+			SetSuggestedTypeText(_minValue, _maxValue);
+		}
+	}
+}
